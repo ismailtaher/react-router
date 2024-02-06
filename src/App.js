0d506e1bc8a50daa0800c3df1b887a3518ebeed5
ismailtaher <ismail.taher@ymail.com>
2024-02-06 17:45:12 +0500

    3rd Commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NewPost from "./NewPost";
 import PostPage from "./PostPage";
 import About from "./About";
 import Missing from "./Missing";
-import { Route, Switch, useHistory } from "react-router-dom";
+import { Route, Routes, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function App() {
@@ -14,11 +14,13 @@ function App() {
     <div className="App">
       <Header></Header>
       <Nav></Nav>
-      <Home></Home>
-      <NewPost></NewPost>
-      <PostPage></PostPage>
-      <About></About>
-      <Missing></Missing>
+      <Routes>
+        <Route exact path="/" element={<Home></Home>}></Route>
+        <Route exact path="/post" element={<NewPost></NewPost>}></Route>
+        <Route path="/post/:id" element={<PostPage></PostPage>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<Missing></Missing>}></Route>
+      </Routes>
       <Footer></Footer>
     </div>
   );
